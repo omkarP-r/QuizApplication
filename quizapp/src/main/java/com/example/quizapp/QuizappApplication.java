320e13308d0175ae5312177519c7b3ed963b46b7
OMKAR <OMKAR@DESKTOP-OS04I81>
2025-01-15 19:54:53 +0530

    First Commit. Uploading Project

---
+++ quizapp/src/main/java/com/example/quizapp/QuizappApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example.quizapp")
+public class QuizappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizappApplication.class, args);
+	}
+
+}
