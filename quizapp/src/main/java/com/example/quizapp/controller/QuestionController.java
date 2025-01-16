b8366979906e3094caa3031e0ad0c94028119d57
OMKAR <OMKAR@DESKTOP-OS04I81>
2025-01-16 20:12:24 +0530

    Created Entity Controller and Service packages.

--- quizapp/src/main/java/com/example/quizapp/controller/QuestionController.java
+++ quizapp/src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -1,12 +1,18 @@
-package com.example.quizapp;
+package com.example.quizapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.quizapp.service.QuestionService;
+
 @RestController
 @RequestMapping("question")
 public class QuestionController {
+	
+	@Autowired
+	QuestionService qservice;
 	
 	@GetMapping("allQuestions")
 	public String getAllQuestions() {
