3443ed87507ba350977b5c93ed2c17e32e7623e3
OMKAR <omkar29pawar@gmail.com>
2025-01-26 18:29:41 +0530

    executed add question

--- quizapp/src/main/java/com/example/quizapp/controller/QuestionController.java
+++ quizapp/src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +33,7 @@ public class QuestionController {
 		
 	}
 	
+	@PostMapping("add/question")
 	public String addQuestion(Question question) {
 		return qservice.addQuestion(question);
 	}
